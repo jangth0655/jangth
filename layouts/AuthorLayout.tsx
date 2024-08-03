2c9ae8afa068a4c762f2a96e6d2af3fcf403a1fe
TAEHEE <jangth0655@gmail.com>
2024-08-04 01:40:00 +0900

    About avatar path 업데이트

--- layouts/AuthorLayout.tsx
+++ layouts/AuthorLayout.tsx
@@ -23,7 +23,7 @@ export default function AuthorLayout({ children, content }: Props) {
           <div className="flex flex-col items-center space-x-2 pt-8">
             {avatar && (
               <Image
-                src={`/jangth/${avatar}`}
+                src={avatar}
                 alt="avatar"
                 width={192}
                 height={192}
